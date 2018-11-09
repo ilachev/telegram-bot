9ba7392206a86278ee711d43939c70eba7bf6ab6
ilya <ilyakarachov@gmail.com>
2018-11-09 17:44:23 +0300

    EH-102 обновил логгер

--- notify-test/public/app/bot/Logger.php
+++ notify-test/public/app/bot/Logger.php
@@ -10,6 +10,10 @@ class Logger
 {
     public static function log($message, $data = null, $error = null)
     {
+        if (!file_exists(__DIR__ . '/../../logs/app.log')) {
+            fopen(__DIR__ . '/../../logs/app.log', 'a');
+        }
+
         if (is_writable(__DIR__ . '/../../logs/app.log')) {
             // Create some handlers
             $stream = new StreamHandler(__DIR__. '/../../logs/app.log', Log::DEBUG);
